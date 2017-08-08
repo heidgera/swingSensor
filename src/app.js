39c39a51b593431702ebc32887d95b40e3b5ac68
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-08 16:38:06 -0500

    possibly working swing sensors

--- src/app.js
+++ src/app.js
@@ -1,60 +1,18 @@
 'use strict';
 
-var obtains = ['µ/midi.js', './src/scope.js', './src/synth.js']; //, 'µ/piFig/piFig.js'
+var obtains = ['µ/swingSensor.js', 'µ/piFig/piFig.js'];
 
-obtain(obtains, (midi, scope, synth, piFig)=> {
+obtain(obtains, ({ swing }, piFig)=> {
   exports.app = {};
 
-  var audio = synth.audioCtx;
-  var notes = synth.synth.notes;
-  var harmonics = synth.harmonics;
-
-  var analyser = audio.createAnalyser();
-  analyser.fftSize = 2048;
-
-  var analyserNote = null;
-
   exports.app.start = ()=> {
-    midi.in.setNoteHandler((note, vel)=> {
-      if (note >= 48 && note < 80) {
-        vel = vel / 256;
+    var pollInt = setInterval(()=> {
+      swing.idle();
+    }, 50);
 
-        notes[note - 48].set(note);
-
-        if (vel) {
-          notes[note - 48].attack(vel);
-
-          // analyser
-          if (analyserNote) analyserNote.gain.disconnect(analyser);
-          analyserNote = notes[note - 48];
-          analyserNote.gain.connect(analyser);
-        } else {
-          notes[note - 48].decay();
-        }
-      }
-    });
-    midi.in.setControlHandler((note, vel)=> {
-      if (note > 2) {
-        /*if (note % 2) synth.real[(note + 1) / 2] = (vel) / 128.0;
-        else synth.imag[(note) / 2] = (vel) / 128.0;*/
-        synth.real[note - 1] = (vel) / 128.0;
-
-        synth.synth.regenWaveform();
-
-        //harmonics.setLevel(note - 3, vel / 128.);
-      } else if (note == 1) synth.synth.setAttack((vel) / 128.0);
-      else if (note == 2) synth.synth.setDecay(3 * (vel) / 128.0);
-    });
-
-    var oscScope = new scope.scope(analyser, µ('#scope'));
-
-    function draw() {
-      oscScope.draw();
-      requestAnimationFrame(draw);
-    }
-
-    draw();
-
+    var pollInt = setInterval(()=> {
+      console.log(swing.point);
+    }, 500);
     console.log('started');
   };
 
---
+++ src/swingSensor.js
@@ -0,0 +1,58 @@
+obtain(['hx711', 'µ/utilities.js', 'fs'], (HX711, { averager }, fs)=> {
+  var threeCells = function() {
+    var _this = this;
+    var numCells = 3;
+    _this.cells = [];
+    _this.cells[0] = new HX711(0, 1);
+    _this.cells[1] = new HX711(8, 9);
+    _this.cells[2] = new HX711(15, 16);
+
+    _this.point = { x: 0, y:0 };
+
+    var aves = [];
+    for (var i = 0; i < numCells; i++) {
+      aves = new averager();
+    }
+
+    var initReads = [0, 0, 0];
+
+    var confDir = './sensorCalibrations.json';
+    var calib = { scaleFactors: [] };
+    if (fs.existsSync(confDir)) {
+      let data = fs.readFileSync(confDir); //file exists, get the contents
+      calib = JSON.parse(data);
+    }
+
+    for (var i = 0; i < _this.cells.length; i++) {
+      if (calib.scaleFactors[i])  _this.cells[i].setScale(calib.scaleFactors[i]);
+      initReads[i] = _this.cells[i].read();
+      _this.cells[i].tare();
+    }
+
+    _this.calibrate = ()=> {
+      for (let i = 0; i < _this.cells.length; i++) {
+        let newVal = _this.cells[i].read();
+        calib.scaleFactors[i] = 3 * (newVal - initReads[i]) / 25;
+        _this.cells[i].setScale(calib.scaleFactors[i]);
+        fs.writeFileSync(confDir, JSON.stringify(calib));
+      }
+    };
+
+    //_this.onNewPoint = ()=> {};
+
+    _this.idle = ()=> {
+      let tot = 0;
+      for (let i = 0; i < _this.cells.length; i++) {
+        aves[i].addSample(_this.cells[i].getUnits(1));
+        tot += aves[i].ave;
+      }
+
+      _this.point.x = ((-.866 * aves[0].ave) + (0.866 * aves[1].ave) + (0 * aves[2].ave)) / tot;
+      _this.point.y = ((.5 * aves[0].ave) + (.5 * aves[1].ave) + (-1 * aves[2].ave)) / tot;
+    };
+  };
+
+  exports.swing = new threeCells();
+
+  provide(exports);
+});
