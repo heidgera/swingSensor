872f46a168397bb51a23f9095b915884c0dd61bc
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-09 12:50:27 -0500

    hx711 library fixes

--- src/app.js
+++ src/app.js
@@ -7,11 +7,6 @@ obtain(obtains, ({ swing }, piFig)=> {
 
   exports.app.start = ()=> {
     var pollInt = setInterval(()=> {
-      swing.idle();
-    }, 50);
-
-    var pollInt = setInterval(()=> {
-      console.log('new point');
       console.log(swing.point);
     }, 500);
     console.log('started');
