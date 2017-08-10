5e3ff392c1282f1361e158091dd96543afce1bc4
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-10 16:08:17 -0500

    testing new library

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ obtain(obtains, ({ swing }, piFig)=> {
   exports.app.start = ()=> {
     var pollInt = setInterval(()=> {
       console.log(swing.point.x + ' | ' + swing.point.y);
-      µ('#track').style.left = (µ('#outer').offsetWidth / 2 + µ('#outer').offsetWidth * swing.point.x + 5) + 'px';
-      µ('#track').style.top = (µ('#outer').offsetHeight / 2 + µ('#outer').offsetHeight * swing.point.y + 5) + 'px';
+      µ('#track').style.left = (200 + 200 * swing.point.x + 5) + 'px';
+      µ('#track').style.top = (200 + 200 * swing.point.y + 5) + 'px';
     }, 50);
     console.log('started');
 
