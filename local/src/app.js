765f10df494fdab357a49181de9c3123bc7afae8
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-15 17:15:19 -0500

    finalize remote control code

--- local/src/app.js
+++ local/src/app.js
@@ -8,7 +8,10 @@ obtain(obtains, ({ swing }, { clamp })=> {
   var setupAudioControl = (cfg)=> {
     if (cfg) {
       var tracks = cfg.tracks.map(el=> new Audio(el));
-      tracks.forEach((el)=> {el.play();});
+      tracks.forEach((el)=> {
+        el.loop = true;
+        el.play();
+      });
       var pollInt = setInterval(()=> {
         cfg.ctrlFunc(swing.point.x, swing.point.y, tracks);
         µ('#track').style.left = (µ('#outer').offsetWidth / 2 + µ('#outer').offsetWidth * swing.point.x + 5) + 'px';
@@ -30,7 +33,7 @@ obtain(obtains, ({ swing }, { clamp })=> {
       setupAudioControl({
         tracks: ['./audio/chimes.mp3'],
         ctrlFunc: (x, y, audio)=> {
-          let dist = clamp(Math.sqrt(x * x + y * y)*2, 0, 1);
+          let dist = clamp(Math.sqrt(x * x + y * y) * 2, 0, 1);
           audio[0].volume = Math.pow(1 - dist, 2);
         },
       });
