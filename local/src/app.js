bf8c24547d9f1e337a7e611375895d846d0f462c
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-16 14:30:23 -0500

    new remote control

--- local/src/app.js
+++ local/src/app.js
@@ -34,7 +34,7 @@ obtain(obtains, ({ swing }, { clamp })=> {
           let dist = clamp(Math.sqrt(x * x + y * y) * 2, 0, 1);
           audio[0].volume = Math.pow(1 - dist, 2);
         },
-        pollTime: 100;
+        pollTime: 100,
       });
     });
 
