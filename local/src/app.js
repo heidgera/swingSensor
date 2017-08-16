bcbf073a6763024c215a8c86d9dd4c3c65541499
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-16 09:16:30 -0500

    testing remote server

--- local/src/app.js
+++ local/src/app.js
@@ -19,7 +19,9 @@ obtain(obtains, ({ swing }, { clamp })=> {
 
   exports.app.start = ()=> {
     var req = get('http://172.17.68.120/controlConfig.js').then((res)=> {
-      setupAudioControl(eval('()=>{ return\n' + res.responseText + '}')());
+      var ctrlCfg = eval('()=>{ return\n' + res.responseText + '}')();
+      console.log(ctrlCfg);
+      setupAudioControl(ctrlCfg);
     }, (error)=> {
       setupAudioControl({
         tracks: ['./audio/chimes.mp3'],
