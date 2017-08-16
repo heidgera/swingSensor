73847721e785b11c061695f56ffc4ddc6c405d88
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-16 14:19:12 -0500

    new remote control

--- client/controlConfig.js
+++ client/controlConfig.js
@@ -1,11 +1,28 @@
 {
-  tracks: ['http://192.168.0.1/audio/chimes.mp3'],
+  tracks: [
+    'http://192.168.0.1/audio/chime1.mp3',
+    'http://192.168.0.1/audio/chime2.mp3',
+    'http://192.168.0.1/audio/chime3.mp3',
+    'http://192.168.0.1/audio/chime4.mp3'
+  ],
   setupFunc: (el, ind, arr)=> {
-    el.loop = true;
-    el.play();
+    el.loop = false;
+    el.pause();
+    el.distCompare = (x, y, pt)=>{
+      let dist = Math.sqrt(Math.pow(x - pt.x, 2) + Math.pow(y - pt.y, 2));
+      if(dist < .25 && !el.struck){
+        el.struck = true;
+        el.currentTime = 0;
+        el.play();
+      } else if( dist > .375){
+        el.struck = false;
+      }
+    }
   },
   ctrlFunc: (x, y, audio)=> {
-    let dist = clamp(Math.sqrt(x * x + y * y) * 2, 0, 1);
-    audio[0].volume = Math.pow(dist, 2);
+    var chimeSpace = (2 * Math.PI) / audio.length;
+    for (var i = 0; i < audio.length; i++) {
+      audio[i].distCompare(x,y,{x: Math.cos(chimeSpace * i), y: Math.sin(chimeSpace * i)})
+    }
   },
 }
